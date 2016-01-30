bd76bb2ce63f18a972c3faecb6f53d8e93ab06d7
Andrew Monks <a@monks.co>
2016-01-30 14:36:38 -0600

    download the feeds

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict'
 
+var fs = require('fs')
+var request = require('request')
 var blogs = require('./art-blogs.json')
 
 for (var blog of blogs) {
   console.log(blog.url)
+  request(blog.url).pipe(fs.createWriteStream('feeds/' + blog.name))
 }
 
