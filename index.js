6dc7aea30f2417a3c7b7334330b5c2f578f6b439
Andrew Monks <a@monks.co>
2016-01-30 14:56:36 -0600

    dates and stuff

--- index.js
+++ index.js
@@ -3,9 +3,22 @@
 var fs = require('fs')
 var request = require('request')
 var blogs = require('./art-blogs.json')
+var date_format = require('date-format')
+
+var now = new Date()
+
 
 for (var blog of blogs) {
   console.log(blog.url)
-  request(blog.url).pipe(fs.createWriteStream('feeds/' + blog.name))
+  var date_dir = 'feeds/' + date
+  if (!fs.existsSync(date_dir)) {
+    fs.mkdirSync(date_dir)
+  }
+  var dir = date_dir + '/' + blog.name
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
+  }
+  var date = date_format(now, 'isoDateTime')
+  request(blog.url).pipe(fs.createWriteStream(dir + '/feed.rss'))
 }
 
