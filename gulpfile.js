ba987ee1f308fed3b5a452cd7a1c0e5fee0ba5aa
Andrew Monks <a@monks.co>
2016-02-05 15:14:42 -0600

    lambdized

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 const rename = require('gulp-rename')
+const install = require('gulp-install')
 const zip = require('gulp-zip')
 
 gulp.task('build', ['pub', 'deps'], function () {
@@ -13,17 +14,18 @@ gulp.task('build', ['pub', 'deps'], function () {
 })
 
 gulp.task('pub', function (cb) {
-  return gulp.src('./pub/*')
+  return gulp.src('./pub/**/*')
          .pipe(gulp.dest('build'))
 })
 
 gulp.task('deps', function (cb) {
-  return gulp.src('./node_modules')
+  return gulp.src('./package.json')
          .pipe(gulp.dest('build'))
+         .pipe(install({production: true}))
 })
 
 gulp.task('zip', ['build'], function (cb) {
-  return gulp.src('build/*')
+  return gulp.src('build/**/*')
          .pipe(zip('aws.zip'))
          .pipe(gulp.dest('dist'))
 })
--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
+require('babel-polyfill')
+
 const AWS = require('aws-sdk')
 const request = require('request')
 const blogs = require('./art-blogs.json')
 const date_format = require('date-format')
 
+let s3 = new AWS.S3()
+
+const gen_key = function (date, blog_name) {
+  let date_dir = 'feeds/' + date
+  let dir = date_dir + '/' + blog_name
+  let key = (dir + '/feed.rss')
+  return key
+}
+
 exports.handler = function (event, context) {
   console.log('just called. event:', event, 'context:', context)
 
   let now = new Date()
+  let date = date_format(now, 'isoDateTime')
+
+  console.log('gonna do the blogs. there are', blogs.length)
 
   for (let blog of blogs) {
-    let date = date_format(now, 'isoDateTime')
-    console.log(date, blog.url, blog.name)
+    console.log('doing a blog', date, blog.name)
 
-    let date_dir = 'feeds/' + date
-    let dir = date_dir + '/' + blog.name
-    let key = (dir + '/feed.rss')
+    let key = gen_key(date, blog.name)
 
-    let s3 = new AWS.S3()
     request(blog.url, function (err, res, body) {
       if (err) {
-        console.log(err)
+        context.fail(err)
         return
       }
       let params = {
@@ -28,7 +38,11 @@ exports.handler = function (event, context) {
         Body: body
       }
       s3.upload(params, function (err, data) {
-        console.log(err, data)
+        if (err) {
+          context.fail(err)
+        } else {
+          context.succeed(data)
+        }
       })
     })
   }
