1df681da1ba41a525eff09ce3f1a75e43eb8057a
Andrew Monks <a@monks.co>
2016-02-05 13:50:18 -0600

    lambdized

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 'use strict'
 
-var fs = require('fs')
+var AWS = require('aws-sdk')
 var request = require('request')
 var blogs = require('./art-blogs.json')
 var date_format = require('date-format')
 
-var now = new Date()
+exports.handler = function () {
+  var now = new Date()
 
-for (var blog of blogs) {
-  var date = date_format(now, 'isoDateTime')
-  console.log(date, blog.url, blog.name)
-  var date_dir = 'feeds/' + date
-  if (!fs.existsSync(date_dir)) {
-    fs.mkdirSync(date_dir)
+  for (var blog of blogs) {
+    var date = date_format(now, 'isoDateTime')
+    console.log(date, blog.url, blog.name)
+
+    var date_dir = 'feeds/' + date
+    var dir = date_dir + '/' + blog.name
+    var key = (dir + '/feed.rss')
+
+    var s3 = new AWS.S3()
+    request(blog.url, function (err, res, body) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      var params = {
+        Bucket: 'art-data',
+        Key: key,
+        Body: body
+      }
+      s3.upload(params, function (err, data) {
+        console.log(err, data)
+      })
+    })
   }
-  var dir = date_dir + '/' + blog.name
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir)
-  }
-  request(blog.url).pipe(fs.createWriteStream(dir + '/feed.rss'))
 }
 
